77553e3322d53efa129df1c1ac8daa63bb048ede
Logan-Stezowski-Student <lost2db@student.oaisd.org>
2024-10-21 17:21:13 +0000

    Finished Switch

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Your Number");
+        int number = int.Parse(Console.ReadLine());
+        switch (number)
+        {
+            case 1:
+                Console.WriteLine("Your number is one.");
+                break;
+            case 2:
+                Console.WriteLine("Your number is two.");
+                break;
+            case 3:
+                Console.WriteLine("Your number is three.");
+                break;
+            case 4:
+                Console.WriteLine("Your number is four.");
+                break;
+            case 5:
+                Console.WriteLine("Your number is five.");
+                break;
+            case 6:
+                Console.WriteLine("Your number is six.");
+                break;
+            case 7:
+                Console.WriteLine("Your number is seven.");
+                break;
+            case 8:
+                Console.WriteLine("Your number is eight.");
+                break;
+            case 9:
+                Console.WriteLine("Your number is nine.");
+                break;
+            case 10:
+                Console.WriteLine("Your number is ten.");
+                break;
+        }
+        if (number > 10)
+        {
+            Console.WriteLine("That number is out of range.");
+        }
+    }
+}
